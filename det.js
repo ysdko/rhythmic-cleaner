20e6b86c5120b0fd6f336db57e5d2ae91cb58ac2
hidemac <hidemac@hidemacnoMacBook-Pro.local>
2021-07-19 20:18:36 +0900

    second commit

--- det.js
+++ det.js
@@ -1,8 +1,30 @@
-window.addEventListener("deviceorientation", handleOrientation, true);
- 
-function handleOrientation(event) {
-  var absolute = event.absolute;
-  var alpha    = event.alpha;
-  var beta     = event.beta;
-  var gamma    = event.gamma;
+function ohayou() {
+    alert("Hello!");
 }
+
+// window.addEventListener("deviceorientation", handleOrientation, true);
+
+// function handleOrientation(event) {
+
+//     var absolute = event.absolute;
+//     var alpha    = event.alpha;
+//     var beta     = event.beta;
+//     var gamma    = event.gamma;
+
+//     var message1 = 'alpha = ${alpha}'
+//     var message2 = 'beta  = ${beta}'
+//     var message3 = 'gamma = ${gamma}'
+
+//     $("#tBox1").val(message1);
+//     $("#tBox2").val(message2);
+//     $("#tBox3").val(message3);
+
+//   }
+
+// $("#btn").on("click", function() {
+
+//     alert('アラートのメッセージ');
+
+// }
+
+// );
--- listen_server.js
+++ listen_server.js
@@ -1,15 +1,25 @@
-// nodeのコアモジュールのhttpを使う
+// nodeのコアモジュール(オブジェクト)のhttpを使う
 var http = require('http');
-var html = require('fs').readFileSync('./listen_server.html');
+//外部から読み込むファイル
+var html = require('fs').readFileSync('./test2.html');
+//httpサーバオブジェクト生成
+//必要な設定をした後に実行する
 var server = http.createServer();
+//サーバにrequestが届いた時の処理を別で記述
 
+
+//第一引数はイベント名、第二引数は組み込む関数
+//requestイベントにfunction関数を割り当てている
+//すなわち、ブラウザからサーバにアクセスした時のサーバの処理を組み込む
 server.on('request', function(req, res) {
     res.writeHead(200, {'Content-Type' : 'text/html'});
     //res.write('hello world');
-    res.end(html);
+    res.write(html);
+    res.end();
 });
 
 // サーバを待ち受け状態にする
 // 第1引数: ポート番号
 // 第2引数: IPアドレス
 server.listen(3000);
+console.log('Server running!');
