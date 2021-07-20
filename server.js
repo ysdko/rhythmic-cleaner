8b645ccbbceb6e49e164899fff010b84d335b49f
ysdko <koki.yoshida110@gmail.com>
2021-07-21 01:55:24 +0900

    phina読み込み

--- server.js
+++ server.js
@@ -6,21 +6,29 @@ server.on('request', getJs);
 server.listen(3000);
 console.log('Server running …');
 function getJs(req, res) {
-  //let url = req.url;
+  let url = req.url;
   //console.log(url);
   //左辺にはurl以下のディレクトリを書く
-  //if ('/' == url) {
+  if ('/' == url) {
     //dataはhtmlの内容が入っているreadFile()の2引数目
     fs.readFile('device_motion.html', 'UTF-8', function (err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       res.end();
     });
-  //} 
-  // else if ('/ver1js.js' == url) {
-  //   fs.readFile('ver1js.js', 'UTF-8', function (err, data) {
-  //     res.writeHead(200, {'Content-Type': 'text/plain'});
-  //     res.write(data); 
-  //     res.end();
-  //   });} 
+  }else if ('/main.js' == url) {
+    fs.readFile('main.js', 'UTF-8', function (err, data) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(data); 
+      res.end();
+    });
+  }else if ('/phina' == url) {
+        fs.readFile('index.html', 'UTF-8', function (err, data) {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          res.end();
+      });} 
+    
+    
+
 }
