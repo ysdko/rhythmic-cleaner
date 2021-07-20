85466150f0ff34869eb206cfead1c9c6f0446e6a
hidemac <hidemac@hidemacnoMacBook-Pro.local>
2021-07-21 02:50:29 +0900

    音声再生(未実装)

--- server.js
+++ server.js
@@ -6,21 +6,25 @@ server.on('request', getJs);
 server.listen(3000);
 console.log('Server running …');
 function getJs(req, res) {
-  //let url = req.url;
-  //console.log(url);
-  //左辺にはurl以下のディレクトリを書く
-  //if ('/' == url) {
-    //dataはhtmlの内容が入っているreadFile()の2引数目
-    fs.readFile('device_motion.html', 'UTF-8', function (err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  //} 
-  // else if ('/ver1js.js' == url) {
-  //   fs.readFile('ver1js.js', 'UTF-8', function (err, data) {
-  //     res.writeHead(200, {'Content-Type': 'text/plain'});
-  //     res.write(data); 
-  //     res.end();
-  //   });} 
+let url = req.url;
+//console.log(url);
+//左辺にはurl以下のディレクトリを書く
+if ('/' == url) {
+  //dataはhtmlの内容が入っているreadFile()の2引数目
+  fs.readFile('device_motion.html', 'UTF-8', function (err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  })}
+  else if ('/tamborine.mp3' == url) {
+    var returnData = {};
+
+    fs.readFile('tamborine.mp3', function(err, file){
+        var base64File = new Buffer(file, 'binary').toString('base64');
+
+        returnData.fileContent = base64File;
+
+        res.json(returnData);
+    })
+  }
 }
