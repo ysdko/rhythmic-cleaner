93ffa87bbb1ba9b9a5a5b1dfb6c756fa95a7fd83
ysdko <koki.yoshida110@gmail.com>
2021-07-21 18:35:24 +0900

    ディレクトリ配下の音を鳴らす

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
-// phina.js をグローバル領域に展開
-phina.globalize();
+// // phina.js をグローバル領域に展開
+// phina.globalize();
 
-// MainScene クラスを定義
-phina.define('MainScene', {
-  superClass: 'CanvasScene',
-  init: function() {
-    this.superInit();
-    // 背景色を指定
-    this.backgroundColor = '#444';
-    // ラベルを生成
-    this.label = Label('Hello, phina.js!').addChildTo(this);
-    this.label.x = this.gridX.center(); // x 座標
-    this.label.y = this.gridY.center(); // y 座標
-    this.label.fill = 'white'; // 塗りつぶし色
-  },
-});
+// // MainScene クラスを定義
+// phina.define('MainScene', {
+//   superClass: 'CanvasScene',
+//   init: function() {
+//     this.superInit();
+//     // 背景色を指定
+//     this.backgroundColor = '#444';
+//     // ラベルを生成
+//     this.label = Label('Hello, phina.js!').addChildTo(this);
+//     this.label.x = this.gridX.center(); // x 座標
+//     this.label.y = this.gridY.center(); // y 座標
+//     this.label.fill = 'white'; // 塗りつぶし色
+//   },
+// });
 
-// メイン処理
-phina.main(function() {
-  // アプリケーション生成
-  var app = GameApp({
-    startLabel: 'main', // メインシーンから開始する
+// // メイン処理
+// phina.main(function() {
+//   // アプリケーション生成
+//   var app = GameApp({
+//     startLabel: 'main', // メインシーンから開始する
+//   });
+//   // アプリケーション実行
+//   app.run();
+// });
+
+function loadFile(){
+  socket.get('/directory/readFile', function(response){
+      var audioSrc = 'data:audio/mp3;base64,' + response.fileContent;
+      var audio = new Audio();
+
+      audio.src = audioSrc;
+      audio.load();
+      audio.play();
   });
-  // アプリケーション実行
-  app.run();
-});
+}
--- server.js
+++ server.js
@@ -27,8 +27,8 @@ function getJs(req, res) {
           res.writeHead(200, {'Content-Type': 'text/html'});
           res.write(data);
           res.end();
-      });} 
-    
+      });
+  }  
     
 
 }
