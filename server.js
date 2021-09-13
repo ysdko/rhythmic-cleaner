a5334bde7383a03965e94f694969c4b897159fd0
hidemac <hidemac@hidemacnoMacBook-Pro.local>
2021-09-13 23:14:32 +0900

    resultScene.jsの追加

--- server.js
+++ server.js
@@ -1,70 +1,105 @@
-let http = require('http');
-let fs = require('fs');
-let server = http.createServer();
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer();
 
-server.on('request', getJs);
+server.on('request', getReq);
 server.listen(3000);
 console.log('Server running …');
-function getJs(req, res) {
+
+function getReq(req, res) {
   let url = req.url;
-  //console.log(url);
+  
   //左辺にはurl以下のディレクトリを書く
-  if ('/' == url) {
+  if ('/' === url) {
     //dataはhtmlの内容が入っているreadFile()の2引数目
-    fs.readFile('index.html', 'UTF-8', function (err, data) {
+    //第一のerrはエラー情報, 第二のdataはファイルの中身
+    //https://www.tuyano.com/index3?id=1126003&page=2
+    fs.readFile('./index.html', 'UTF-8', function (err, data) {
+      if(err) {
+        console.error(err.message);
+        // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+        process.exit(1);
+      }
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       res.end();
     });
   }
-  else if ('/main.js' == url) {
-    fs.readFile('main.js', 'UTF-8', function (err, data) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
+  else if ('/mainScene.js' === url) {
+    fs.readFile('mainScene.js', 'UTF-8', function (err, data) {
+      if(err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
       res.write(data); 
       res.end();
     });
   }
   else if ('/device_motion.js' == url) {
     fs.readFile('device_motion.js', 'UTF-8', function (err, data) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
+      if(err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
       res.write(data); 
       res.end();
     });
   }
   else if ('/config.js' == url) {
     fs.readFile('config.js', 'UTF-8', function (err, data) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(data);
-    res.end();
+      if(err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data); 
+      res.end();
     });
   } 
-  // else if ('/title.png' == url) {
-  //   res.writeHead(200, {
-  //     'Content-Type': `image/png; charset=utf-8`  //　← ここがキモ！
-  //     });
-  //   var image = fs.readFileSync("title.png", "binary"); // ← ファイルpathはその環境に合わせてください
-  //     res.end(image, "binary");
-  // }  
   else if ('/component.js' == url) {
     fs.readFile('component.js', 'UTF-8', function (err, data) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(data);
-    res.end();
+      if(err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data); 
+      res.end();
     });
   }
-  else if ('/titlescene.js' == url) {
-    fs.readFile('titlescene.js', 'UTF-8', function (err, data) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(data);
-    res.end();
+  else if ('/titleScene.js' == url) {
+    fs.readFile('titleScene.js', 'UTF-8', function (err, data) {
+      if(err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data); 
+      res.end();
+    });
+  }
+  else if ('/resultScene.js' == url) {
+    fs.readFile('resultScene.js', 'UTF-8', function (err, data) {
+      if(err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data); 
+      res.end();
     });
   }
   else if ('/clock.json' == url) {
     fs.readFile('clock.json', 'UTF-8', function (err, data) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(data);
-    res.end();
+      if(err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.write(data); 
+      res.end();
     });
   }
-
 }
