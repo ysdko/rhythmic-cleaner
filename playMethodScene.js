b6af637f3a9d58db119a3cd738eceba4388e5b3d
ysdko <koki.yoshida110@gmail.com>
2021-09-26 16:50:40 +0900

    fix 遊び画面修正

--- playMethodScene.js
+++ playMethodScene.js
@@ -29,13 +29,15 @@ phina.define("PlayMethodScene", {
       strokeWidth: 3,
     }).addChildTo(playMethodGroup);
 
+    Sprite('play_image').addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(5.5)).setScale(0.3, 0.3);
+
     const textGroup = DisplayElement()
-      .setPosition(this.gridX.center(), this.gridY.span(10.8))
-      .addChildTo(this);
+    .setPosition(this.gridX.center(), this.gridY.span(11.3))
+    .addChildTo(this);
 
     RectangleShape({
       width: 600,
-      height: 300,
+      height: 270,
       fill: "black",
       stroke: "cyan",
       strokeWidth: 10,
@@ -47,7 +49,7 @@ phina.define("PlayMethodScene", {
     Label({
       text: "1. スマホを掃除機に取り付けよう",
       fill: "white",
-      fontSize: 30,
+      fontSize: 25,
       align: "left",
     })
       .setPosition(-280, -100)
@@ -56,7 +58,7 @@ phina.define("PlayMethodScene", {
     Label({
       text: "2. ノーツに合わせて掃除機をスライド\n    させよう",
       fill: "white",
-      fontSize: 30,
+      fontSize: 25,
       align: "left",
     })
       .setPosition(-280, 0)
@@ -65,7 +67,7 @@ phina.define("PlayMethodScene", {
     Label({
       text: "3. 掃除機の音がうるさい場合はイヤホン\n    をつけよう",
       fill: "white",
-      fontSize: 30,
+      fontSize: 25,
       align: "left",
     })
       .setPosition(-280, 100)
